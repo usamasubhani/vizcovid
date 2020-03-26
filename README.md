a35f8354ec7d293cdf3ec86d1430e0ea9b0d1967
Usama Subhani <reachusama@protonmail.com>
2020-03-26 22:00:34 +0500

    Major work, done on stream

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # vizcovid
+Vizualize COVID-19 Data using VueJS and https://covidapi.info/ api.
+
+## Features
+- Global report
+- Country report
+- Map Maybe
+
+## To-do
+
+- [x] Tables
+- [ ] Create Graphs
+- [ ] Add more countries
--- app.js
+++ app.js
@@ -1,6 +1,77 @@
-new Vue({
+
+  
+
+var vapp = new Vue({
     el: '#vue-app',
     data: {
-        message: 'Hello World!'
+        message: 'Loading',
+        rawJson: '',
+        selected: 'PAK',
+        countries: [
+            'PAK',
+            'IND',
+            'AFG'
+        ],
+        latest: ''
+    },
+    created: function() {
+        this.getCountryData();
+    },
+    methods: {
+        getGlobalData : function() {
+            fetch('https://covidapi.info/api/v1/global')
+            .then(response => response.json())
+            .then(data => {
+                this.message = data
+            })
+        },
+        getCountryData: function(country) {
+            fetch(`https://covidapi.info/api/v1/country/${this.selected}`)
+            .then(response => response.json())
+            .then(data => {
+                this.message = data
+                this.rawJson = data
+                this.latest = this.rawJson.result[Object.keys(this.rawJson.result)[Object.keys(this.rawJson.result).length-1]]
+                // console.log(this.rawJson.result[0].values())
+                
+            })
+        }
     }
 });
+
+let wow = function(rawJson){
+    var tempArray = []
+    for (let [key, value] of Object.entries(vapp.rawJson.result)) {
+        tempArray.push(value.confirmed)
+    }
+    console.log(tempArray)
+    return tempArray
+}
+
+
+Vue.component('dc', {
+    extends: VueChartJs.Line,
+    data() {
+        return {
+          componentKey: 0,
+        };
+      },
+    mounted () {
+      this.renderChart({
+        labels: Object.keys(vapp.rawJson.result),
+        datasets: [
+          {
+            label: 'Confirmed Cases',
+            backgroundColor: '#f87979',
+            data: wow()
+          }
+        ]
+      }, {responsive: true, maintainAspectRatio: false})
+    },
+    methods: {
+        forceRerender() {
+          this.componentKey += 1;  
+        }
+      }
+  })
+
