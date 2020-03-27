f56df575e4a2c309a669109473469f42e42ef5a8
Usama Subhani <reachusama@protonmail.com>
2020-03-27 23:45:08 +0500

    Update chart on country change

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 
-  
-
 var vapp = new Vue({
     el: '#vue-app',
     data: {
@@ -10,9 +8,12 @@ var vapp = new Vue({
         countries: [
             'PAK',
             'IND',
-            'AFG'
+            'AFG',
+            'ITA',
+            'USA'
         ],
-        latest: ''
+        latest: '',
+        dataChart: [10, 39, 10, 40, 39, 0, 0]
     },
     created: function() {
         this.getCountryData();
@@ -32,9 +33,13 @@ var vapp = new Vue({
                 this.message = data
                 this.rawJson = data
                 this.latest = this.rawJson.result[Object.keys(this.rawJson.result)[Object.keys(this.rawJson.result).length-1]]
+                this.dataChart = wow()
                 // console.log(this.rawJson.result[0].values())
                 
             })
+        },
+        changeData: function() {
+          this.dataChart = [6, 6, 3, 5, 5, 6];
         }
     }
 });
@@ -51,27 +56,89 @@ let wow = function(rawJson){
 
 Vue.component('dc', {
     extends: VueChartJs.Line,
-    data() {
-        return {
-          componentKey: 0,
-        };
-      },
+    props: ["data"],
+    // data() {
+    //     return {
+    //       datacollection: null
+    //     };
+    //   },
     mounted () {
-      this.renderChart({
+      
+      this.getData()
+      console.log(this.chartData)
+      // this.renderChart()
+      
+    },
+    computed: {
+      chartData: function() {
+        console.log(this.data)
+        return this.data;
+      }
+    },
+    methods: {
+        getData() {
+          this.renderChart({
+            labels: Object.keys(vapp.rawJson.result),
+            datasets: [
+              {
+                label: 'Confirmed Cases',
+                backgroundColor: '#f87979',
+                data: this.chartData
+              }
+            ]
+        }),{responsive: true, maintainAspectRatio: false };
+      }
+    },
+    watch:{
+      data: function() {
+        this._chart.destroy();
+        //this.renderChart(this.data, this.options);
+        this.getData();
+      }
+    }
+})
+
+
+
+Vue.component("line-chart", {
+  extends: VueChartJs.Line,
+  props: ["data", "options"],
+  mounted() {
+    this.renderLineChart();
+  },
+  computed: {
+    chartData: function() {
+      console.log(this.data)
+      return this.data;
+    }
+  },
+  methods: {
+    renderLineChart: function() {
+    this.renderChart(
+      {
         labels: Object.keys(vapp.rawJson.result),
         datasets: [
           {
-            label: 'Confirmed Cases',
-            backgroundColor: '#f87979',
-            data: wow()
+            label: "Confirmed",
+            // backgroundColor: "#f87979",
+            borderColor: '#FC2525', 
+            pointBackgroundColor: 'red', 
+            borderWidth: 1, 
+            pointBorderColor: 'black',
+            data: this.chartData
           }
         ]
-      }, {responsive: true, maintainAspectRatio: false})
-    },
-    methods: {
-        forceRerender() {
-          this.componentKey += 1;  
-        }
-      }
-  })
+      },
+      { responsive: true, maintainAspectRatio: false }
+    );      
+    }
+  },
+  watch: {
+    data: function() {
+      // this._chart.destroy();
+      //this.renderChart(this.data, this.options);
+      this.renderLineChart();
+    }
+  }
+});
 
